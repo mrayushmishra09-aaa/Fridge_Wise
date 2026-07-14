642e3b16381d8e2947863877d8710b4d4ca7b8f9
Ayush Mishra <mrayushmishra09@gmail.com>
2026-07-15 01:17:49 +0530

    -Implement Floating Action Button (FAB) in the Inventory fragment to improve user interaction. - Initialize the AddItemFragment layout structure as a placeholder for future development.

--- app/src/main/java/com/example/fridgewise/AddItemFragment.java
+++ app/src/main/java/com/example/fridgewise/AddItemFragment.java
@@ -2,22 +2,18 @@ package com.example.fridgewise;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link inventoryuFragment#newInstance} factory method to
+ * Use the {@link AddItemFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class inventoryuFragment extends Fragment {
+public class AddItemFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +24,7 @@ public class inventoryuFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public inventoryuFragment() {
+    public AddItemFragment() {
         // Required empty public constructor
     }
 
@@ -38,11 +34,11 @@ public class inventoryuFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment inventoryuFragment.
+     * @return A new instance of fragment AddItemFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static inventoryuFragment newInstance(String param1, String param2) {
-        inventoryuFragment fragment = new inventoryuFragment();
+    public static AddItemFragment newInstance(String param1, String param2) {
+        AddItemFragment fragment = new AddItemFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,23 +59,6 @@ public class inventoryuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inventoryu, container, false);
-    }
-    
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
-        super.onViewCreated(view, savedInstanceState);
-        FloatingActionButton fab = view.findViewById(R.id.floatingActionButton);
-        
-        fab.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                getParentFragmentManager().beginTransaction()
-                        .replace(R.id.fragmentContainerView2, new AddItemFragment())
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-        
+        return inflater.inflate(R.layout.fragment_add_item, container, false);
     }
 }
