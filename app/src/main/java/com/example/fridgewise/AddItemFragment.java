6aac46c2d7a889cdbea7764466b1318859110354
Ayush Mishra <mrayushmishra09@gmail.com>
2026-07-25 15:54:27 +0530

    ui: Fixed Back navigation header .

--- app/src/main/java/com/example/fridgewise/AddItemFragment.java
+++ app/src/main/java/com/example/fridgewise/AddItemFragment.java
@@ -1,42 +1,27 @@
 package com.example.fridgewise;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddItemFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AddItemFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // 👇 THIS IS YOUR BACK ARROW VARIABLE
+    private ImageView img_01;
 
     public AddItemFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddItemFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AddItemFragment newInstance(String param1, String param2) {
         AddItemFragment fragment = new AddItemFragment();
         Bundle args = new Bundle();
@@ -58,7 +43,26 @@ public class AddItemFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_item, container, false);
+        // Connect to your XML layout
+        View view = inflater.inflate(R.layout.fragment_add_item, container, false);
+
+        // 👇 SET UP YOUR BACK ARROW BUTTON
+        img_01 = view.findViewById(R.id.back_arrow);
+        img_01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // This goes back to the previous screen
+                if (getActivity() != null) {
+                    getActivity().onBackPressed();
+                }
+            }
+        });
+
+        // If you have other code (like save button, etc.), add it HERE
+        // Example:
+        // Button saveButton = view.findViewById(R.id.saveButton);
+        // saveButton.setOnClickListener(...);
+
+        return view;
     }
 }
